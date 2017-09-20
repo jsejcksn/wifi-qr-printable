97c48c8f38aeab8cbafc13d5360881f02810776a
Jesse Jackson <jsejcksn@users.noreply.github.com>
2017-09-20 17:44:03 -0500

    Initial commit

---
+++ js/wifi-qr-gen.js
@@ -0,0 +1,39 @@
+(() => {
+  'use strict';
+
+  const el = {
+    qrImg: document.getElementById('qr'),
+    ssid: document.getElementById('ssid'),
+    pass: document.getElementById('pass'),
+    hidden: document.getElementById('hidden'),
+    update: document.getElementById('update')
+  };
+
+  const wifi = {
+    ssid: 'ssid',
+    pass: 'password',
+    type: 'WPA',
+    hidden: false
+  };
+
+  el.update.addEventListener('click', updatePrint);
+
+  function tOnly (bool) {
+    return bool ? true : '';
+  }
+
+  function updatePrint () {
+    updateInfo();
+    setTimeout(() => window.print(), 750); // Should be async, but there is no async API. Hopefully 750ms is enough.
+  }
+
+  function updateInfo () {
+    wifi.ssid = el.ssid.value;
+    wifi.pass = el.pass.value;
+    wifi.hidden = el.hidden.checked;
+    const qrUrl = `https://zxing.org/w/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=${encodeURIComponent(`WIFI:S:${wifi.ssid};T:${wifi.type};P:${wifi.pass};${tOnly(wifi.hidden)};`)}`;
+    el.qrImg.src = qrUrl;
+  }
+
+
+})();
